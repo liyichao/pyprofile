720873052a652eb16f978acaae71d47b8af33d30
Li Yichao <lyc@zhihu.com>
2017-01-21 20:46:30 +0800

    Add normal.

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ profile of python programs
 
 ## systemtap-python-tools
 
+* 
 * not merging stacks of different greenlets
---
+++ examples/normal.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def callee_d():
+    j = 0
+    for i in range(100):
+        j += 1
+    return j
+
+
+def callee_a():
+    callee_d()
+
+
+def callee_b():
+    callee_c()
+
+
+def callee_c():
+    callee_d()
+
+
+def caller():
+    callee_a()
+    callee_b()
+    callee_a()
+
+
+while True:
+    caller()
